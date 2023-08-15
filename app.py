e5016f68cb1bbedb9a755aeefe5e177be4eb31d7
EC2 Default User <ec2-user@ip-172-31-92-226.ec2.internal>
2023-08-15 01:43:14 +0000

    git rebase error

--- app.py
+++ app.py
@@ -3,18 +3,33 @@ from pymysql import connections
 import os
 import random
 import argparse
-import boto3
+
+# Import the required libraries
+import requests
+from flask import send_from_directory
+
+  
+
 
 app = Flask(__name__)
 
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "password"
+DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
-BGIMG = os.environ.get("BGIMG") or "background.jpg"
-BUCKETNAME = os.environ.get("BUCKETNAME") or "clo835-g7-finalproject"
-GRPNAME = os.environ.get("GRPNAME") or "Group 7"
-DBPORT = int(os.environ.get("DBPORT", "3306"))
+COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
+IMAGE_URL = os.environ.get("IMAGE_URL") or "Broken IMG"
+GROUP_NAME = os.environ.get("GROUP_NAME")
+
+DBPORT = os.environ.get("DBPORT")
+if DBPORT is not None:
+    try:
+        DBPORT = int(DBPORT)
+    except ValueError:
+        print("Invalid value for DBPORT. Using default port.")
+        DBPORT = 3306
+else:
+    DBPORT = 3306
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
@@ -23,36 +38,42 @@ db_conn = connections.Connection(
     user= DBUSER,
     password= DBPWD, 
     db= DATABASE
+    
 )
-
 output = {}
 table = 'employee';
 
-default_bucket = "clo835-g7-finalproject"
-default_image = "bgimage.jpg"
 
-@app.route("/download", methods=['GET', 'POST'])
-def download(bucket = default_bucket, imageName = default_image):
-    try:
-        imagesDir = "static"
-        if not os.path.exists(imagesDir):
-            os.makedirs(imagesDir)
-        bgImagePath = os.path.join(imagesDir, "bg.png")
-        
-        print(bucket, imageName)
-        s3 = boto3.resource('s3')
-        s3.Bucket(bucket).download_file(imageName, bgImagePath)
-        return os.path.join(imagesDir, "bg.png")
-    except Exception as e:
-        print("Exception occured while fetching the image! Check the log --> ", e)
-       
+
+
+# Define the path where you'll save the downloaded image
+DOWNLOADS_PATH = "static/downloads"
+if not os.path.exists(DOWNLOADS_PATH):
+    os.makedirs(DOWNLOADS_PATH)
+    
+    
+# Download the image from the S3 URL
+IMAGE_PATH = os.path.join(DOWNLOADS_PATH, "sample1.jpg")
+response = requests.get(IMAGE_URL)
+if response.status_code == 200:
+    with open(IMAGE_PATH, "wb") as f:
+        f.write(response.content)
+    print("Image downloaded successfully.")
+else:
+    print("Failed to download image.")
+
+# Define a variable for the image path
+BACKGROUND_IMAGE_PATH = "/static/downloads/sample1.jpg"  
+print(BACKGROUND_IMAGE_PATH)
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', image=image, group_name=GRPNAME)
+    return render_template('addemp.html', background_image=BACKGROUND_IMAGE_PATH, GROUP_NAME=GROUP_NAME)
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', image=image, group_name=GRPNAME)
+    return render_template('about.html', background_image=BACKGROUND_IMAGE_PATH, GROUP_NAME=GROUP_NAME)
     
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -76,11 +97,11 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, image=image, group_name=GRPNAME)
+    return render_template('addempoutput.html', name=emp_name, background_image=BACKGROUND_IMAGE_PATH, GROUP_NAME=GROUP_NAME)
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", image=image, group_name=GRPNAME)
+     return render_template("getemp.html", background_image=BACKGROUND_IMAGE_PATH, GROUP_NAME=GROUP_NAME)
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -95,6 +116,7 @@ def FetchData():
         cursor.execute(select_sql,(emp_id))
         result = cursor.fetchone()
         
+        # Add No Employee found form
         output["emp_id"] = result[0]
         output["first_name"] = result[1]
         output["last_name"] = result[2]
@@ -108,9 +130,8 @@ def FetchData():
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], image=image, group_name=GRPNAME)
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"],  background_image=BACKGROUND_IMAGE_PATH, GROUP_NAME=GROUP_NAME)
 
-if __name__ == '__main__':
-    image = download(BUCKETNAME, BGIMG)
-    print(image)
-    app.run(host='0.0.0.0',port=81,debug=True)
+
+
+app.run(host='0.0.0.0',port=81,debug=True)
